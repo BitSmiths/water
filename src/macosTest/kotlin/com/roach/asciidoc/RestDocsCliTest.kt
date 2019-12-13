720b4b342a254ccee8ab5c8d1ae61ab6e3a2ca9b
steveroach <sroachst@gmail.com>
2019-12-13 18:36:41 -0500

    Should be functional, requires gradle

---
+++ src/macosTest/kotlin/com/roach/asciidoc/RestDocsCliTest.kt
@@ -0,0 +1,12 @@
+package com.roach.asciidoc
+
+import com.bitsmiths.restdocs.hello
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class RestDocsCliTest {
+    @Test
+    fun testHello() {
+        assertTrue("Kotlin/Native" in hello())
+    }
+}
